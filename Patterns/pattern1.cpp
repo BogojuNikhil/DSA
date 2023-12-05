d24be77b3c71d64f9e595fcea53621f119b53a36
Nikhil Bogoju <bogojunikhil@gmail.com>
2023-12-05 21:50:23 +0530

    Add files via upload

---
+++ Patterns/pattern1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a=2;
+  int n;
+  int sum;
+  cout<<"enter a number n";
+  cin>>n;
+  while(a<=n){
+     sum=sum+a;
+     a=a+2;
+  }
+  cout<<sum;
+}
+
